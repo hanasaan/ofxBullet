557caf713d6a56a7949c38e305d60a62e0bdc81a
Yuya Hanai <yuya@rhizomatiks.com>
2018-03-29 14:32:09 +0900

    Clean up unused *.cpp s and libs , updated addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxBullet
+
+common:
+	ADDON_INCLUDES = src
+	ADDON_INCLUDES += src/events
+	ADDON_INCLUDES += src/joints
+	ADDON_INCLUDES += src/render
+	ADDON_INCLUDES += src/shapes
+	ADDON_INCLUDES += libs/bullet2.8.2/include
